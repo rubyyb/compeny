9ad431171761b1842887c839eb08878afbe6d2bb
rubyyb <153253309+rubyyb@users.noreply.github.com>
2024-07-19 13:48:05 +0900

    Add files via upload

---
+++ 회사/vue2.vue
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vue 예제</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
+</head>
+<body>
+    <div id="app">
+        {{ message }}
+    </div>
+
+    <script>
+        var app = new Vue({
+        el: '#app',
+        data: {
+            message: '안녕하세요!'
+        }
+        });
+    </script>   
+</body>
+</html>
